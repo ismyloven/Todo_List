8955208b01549c6ddeb9c68c1ee81a6cffbd3375
Nail Ismailov <dd030789ine@gmail.com>
2020-08-12 01:11:02 +0300

    second

--- src/App.js
+++ src/App.js
@@ -9,92 +9,141 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import uuid from "react-uuid";
 
 
-class App extends Component{
-    state = {
-            items: [],
-            id: uuid(),
-            item: "",
-            editItem: false
+class App extends Component {
+	state = {
+		items: [],
+		id: uuid(),
+		item: "",
+		editItem: false,
 
-    };
-    handleChange = e => {
-        this.setState({
-            item: e.target.value
+	};
+
+
+    handleCkeck = i => {
+        const modifiedItems = this.state.items.map(item => {
+            if (item.id === i.id) {
+                return {...item, isDone: !item.isDone}
+            }
+            return {...item}
         });
-    };
-    handleSubmit = e => {
-        e.preventDefault();
 
-const newItem = {
-    id: this.state.id,
-    title: this.state.item
-};
-
-
-const updatedItems = [...this.state.items,newItem];
-
-this.setState({
-    items: updatedItems,
-    item: "",
-    id: uuid(),
-    editItem: false
-});
-
-    };
-    clearList = ()=>{
         this.setState({
-            items:[]
+            items: modifiedItems
         });
+
+
     };
 
-    handleDelete = id => {
-        const filteredItems = this.state.items.filter(item => item.id !== id);
-        this.setState({
-            items:filteredItems
-        });
-    };
-handleEdit = id =>{
-    const filteredItems = this.state.items.filter(item => item.id !== id);
-
-    const selectedItem = this.state.items.find(item=> item.id ===id);
 
 
-    this.setState({
-        items:filteredItems,
-        item: selectedItem.title,
-        editItem: true,
-        id:id
-    });
-};
-render() {
+	handleFavorite = i => {
+		const modifiedItems = this.state.items.map(item => {
+			if (item.id === i.id) {
+				return {...item, isFavorite: !item.isFavorite}
+			}
+			return {...item}
+		});
+
+		this.setState({
+			items: modifiedItems.sort((x, y) =>
+				(x.isFavorite === y.isFavorite) ? 0 : x.isFavorite ? -1 : 1
+	)
+	});
 
 
-    return (
-    <div className="container">
-        <div className="row">
-            <div className="col-10 mx-auto col-md-8 mt-4">
-                <div className="">
-                    <div className="text-capitalize text-center todoSearch"><span className="todolist">To Do List</span>  < TodoSearch/></div>
+	};
 
-                </div>
-                <Todoinput item={this.state.item}
-                           handleChange={this.handleChange}
-                           handleSubmit={this.handleSubmit}
-                           editItem={this.state.editItem}
-                />
 
-                <TodoList
-                    items={this.state.items}
-                    clearList={this.clearList}
-                    handleDelete={this.handleDelete}
-                    handleEdit={this.handleEdit}
-                />
+	handleChange = e => {
+		this.setState({
+			item: e.target.value
+		});
+	};
+	handleSubmit = e => {
+		e.preventDefault();
+		if (!this.state.item) {
+			return
+		};
 
-            </div>
-        </div>
-    </div>
 
-  );
+
+		const newItem = {
+			id: this.state.id,
+			title: this.state.item,
+			isFavorite: false,
+            isDone: false
+		};
+
+
+		const updatedItems = [...this.state.items, newItem];
+
+		this.setState({
+			items: updatedItems,
+			item: "",
+			id: uuid(),
+			editItem: false
+		});
+
+	};
+	clearList = () => {
+		this.setState({
+			items: []
+		});
+	};
+
+	handleDelete = id => {
+		const filteredItems = this.state.items.filter(item => item.id !== id);
+		this.setState({
+			items: filteredItems
+		});
+	};
+	handleEdit = id => {
+		const filteredItems = this.state.items.filter(item => item.id !== id);
+
+		const selectedItem = this.state.items.find(item => item.id === id);
+
+
+		this.setState({
+			items: filteredItems,
+			item: selectedItem.title,
+			editItem: true,
+			id: id
+		});
+	};
+
+	render() {
+
+
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-10 mx-auto col-md-8 mt-4">
+						<div className="">
+							<div className="text-capitalize text-center todoSearch"><span className="todolist">To Do List</span>
+								< TodoSearch/></div>
+
+						</div>
+						<Todoinput item={this.state.item}
+								   handleChange={this.handleChange}
+								   handleSubmit={this.handleSubmit}
+								   editItem={this.state.editItem}
+						/>
+
+						<TodoList
+							items={this.state.items}
+							clearList={this.clearList}
+							handleDelete={this.handleDelete}
+							handleEdit={this.handleEdit}
+							handleFavorite={this.handleFavorite}
+                            handleCkeck ={this.handleCkeck}
+						/>
+
+					</div>
+				</div>
+			</div>
+
+		);
+	}
 }
-}
+
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import TodoItem from "./TodoItem";
+
 //css style
 
 import "./TodoList/Todolist.css";
@@ -7,15 +8,17 @@ import "./TodoList/Todolist.css";
 
 export default class TodoList extends Component {
 	render() {
-		const {items, clearList, handleDelete, handleEdit} =this.props;
+		const {items, clearList, handleDelete, handleEdit, handleFavorite, handleCkeck} =this.props;
 		return (
 			<ul className="list-group my-5">
 				{items.map(item =>{
 						return <TodoItem
 							key={item.id}
-							title={item.title}
+							item={item}
 							handleDelete={()=> handleDelete(item.id)}
 							handleEdit={()=> handleEdit(item.id)}
+							handleFavorite={()=> handleFavorite(item)}
+							handleCkeck={()=> handleCkeck(item)}
 						/>;
 
 					})}
